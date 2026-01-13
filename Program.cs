a77080150757511c58ef9c26a9ba1f7d7f7dd533
zelinskyiak24-maker <zelinskyi_ak24@nuwm.edu.ua>
2026-01-13 11:56:26 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,88 @@
-﻿using System;
+using System;
 
-namespace LabWork
+class Building
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    public int Floors;
+    public string Material;
+    public bool Elevator;
+    public string Type;
+
+    public void Show()
     {
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Тип будинку: " + Type);
+        Console.WriteLine("Кількість поверхів: " + Floors);
+        Console.WriteLine("Матеріал: " + Material);
+        Console.WriteLine("Ліфт: " + (Elevator ? "Так" : "Ні"));
+        Console.WriteLine("----------------------------");
+    }
+}
+
+class Builder
+{
+    private Building building;
+
+    public Builder()
+    {
+        building = new Building();
+    }
+
+    public void SetType(string type)
+    {
+        building.Type = type;
+    }
+
+    public void SetFloors(int floors)
+    {
+        building.Floors = floors;
+    }
+
+    public void SetMaterial(string material)
+    {
+        building.Material = material;
+    }
+
+    public void SetElevator(bool elevator)
+    {
+        building.Elevator = elevator;
+    }
+
+    public Building GetBuilding()
+    {
+        return building;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Builder officeBuilder = new Builder();
+        officeBuilder.SetType("Офісна будівля");
+        officeBuilder.SetFloors(8);
+        officeBuilder.SetMaterial("Скло");
+        officeBuilder.SetElevator(true);
+
+        Building office = officeBuilder.GetBuilding();
+        office.Show();
+
+        Builder houseBuilder = new Builder();
+        houseBuilder.SetType("Приватний будинок");
+        houseBuilder.SetFloors(2);
+        houseBuilder.SetMaterial("Цегла");
+        houseBuilder.SetElevator(false);
+
+        Building house = houseBuilder.GetBuilding();
+        house.Show();
+
+        Builder flatBuilder = new Builder();
+        flatBuilder.SetType("Багатоквартирний будинок");
+        flatBuilder.SetFloors(16);
+        flatBuilder.SetMaterial("Бетон");
+        flatBuilder.SetElevator(true);
+
+        Building flat = flatBuilder.GetBuilding();
+        flat.Show();
+
+        Console.ReadLine();
     }
 }
