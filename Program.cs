88df06e8d795e6148b6649f39de1ba54f02cb182
zelinskyiak24-maker <zelinskyi_ak24@nuwm.edu.ua>
2026-01-15 22:04:51 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,89 @@
-﻿using System;
+using System;
 
-namespace LabWork
+interface ISocialNetwork
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    void Post(string message);
+}
+
+class ExistingSystem
+{
+    private ISocialNetwork socialNetwork;
+
+    public ExistingSystem(ISocialNetwork socialNetwork)
     {
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Hello World!");
-        }
+        this.socialNetwork = socialNetwork;
+    }
+
+    public void Publish(string text)
+    {
+        socialNetwork.Post(text);
+    }
+}
+
+class FacebookApi
+{
+    public void SendPost(string text)
+    {
+        Console.WriteLine("Facebook | Новий пост успішно опубліковано");
+        Console.WriteLine("Повідомлення: " + text);
+        Console.WriteLine();
+    }
+}
+
+class TwitterApi
+{
+    public void Tweet(string text)
+    {
+        Console.WriteLine("Twitter | Твіт успішно надіслано");
+        Console.WriteLine("Повідомлення: " + text);
+        Console.WriteLine();
+    }
+}
+
+class FacebookAdapter : ISocialNetwork
+{
+    private FacebookApi facebook;
+
+    public FacebookAdapter(FacebookApi facebook)
+    {
+        this.facebook = facebook;
+    }
+
+    public void Post(string message)
+    {
+        facebook.SendPost(message);
+    }
+}
+
+class TwitterAdapter : ISocialNetwork
+{
+    private TwitterApi twitter;
+
+    public TwitterAdapter(TwitterApi twitter)
+    {
+        this.twitter = twitter;
+    }
+
+    public void Post(string message)
+    {
+        twitter.Tweet(message);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        FacebookApi facebookApi = new FacebookApi();
+        TwitterApi twitterApi = new TwitterApi();
+
+        ISocialNetwork facebook = new FacebookAdapter(facebookApi);
+        ISocialNetwork twitter = new TwitterAdapter(twitterApi);
+
+        ExistingSystem system1 = new ExistingSystem(facebook);
+        ExistingSystem system2 = new ExistingSystem(twitter);
+
+        system1.Publish("Сьогоднішні новини вражають!");
+        system2.Publish("Автомобіль моєї мрії!");
     }
 }
